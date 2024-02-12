428dba257147bda0740f33b6f55d54cb83dc79a9
Luka Chemia <lchemia@aldagi.ge>
2024-02-12 15:21:21 +0400

    gridAdd

--- educational_project/src/app/components/home/home.component.ts
+++ educational_project/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
@@ -13,7 +13,10 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
   styleUrl: './home.component.scss',
   
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   images =[
     {name: 'carousel-1.jpg' },
     {name: 'carousel-2.jpg'}
