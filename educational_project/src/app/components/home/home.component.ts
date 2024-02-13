b139c821234a9b4ff1e52ca519927c17a0d2263d
Luka Chemia <lchemia@aldagi.ge>
2024-02-13 18:11:07 +0400

    navBarFix

--- educational_project/src/app/components/home/home.component.ts
+++ educational_project/src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -8,21 +8,52 @@ import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule,NgbCarouselModule],
+  imports: [RouterModule,NgbCarouselModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   
 })
-export class HomeComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+export class HomeComponent  {
+  
   images =[
     {name: 'carousel-1.jpg' },
     {name: 'carousel-2.jpg'}
   ]
 
   showCarousel = true;
+  showMenu: boolean = false;
 
-  
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.checkScreenWidth();
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.checkScreenWidth();
+    }
+  }
+
+  @HostListener('window:scroll')
+  onScroll() {
+    if (isPlatformBrowser(this.platformId) && window.innerWidth <= 850) {
+      // Reset the menu to default position when scrolling
+      this.showMenu = false;
+    }
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+  checkScreenWidth() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Set showMenu based on screen width
+      this.showMenu = window.innerWidth > 850;
+    }
+  }
 }
